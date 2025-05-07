43b90964fa78837ebf5ea0c4a6d686337eca0a0e
jeff283 <njorogejeff2019@gmail.com>
2025-05-07 14:11:30 +0300

    I can't belive typescript let's this through

--- services/useFetch.ts
+++ services/useFetch.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-const useFetch = <T>(fecthFunction: () => Promise<T>, autoFetch: true) => {
+const useFetch = <T>(fecthFunction: () => Promise<T>, autoFetch = true) => {
   const [data, setData] = useState<T | null>(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<Error | null>(null);
