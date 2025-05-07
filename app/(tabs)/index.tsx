50d10c7da249f316e8f0ac8274fe76a8099ebb25
jeff283 <njorogejeff2019@gmail.com>
2025-05-07 14:18:20 +0300

    Increase visibility

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,10 @@
 import SearchBar from "@/components/SearchBar";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
+import { fetchMovies } from "@/services/api";
+import useFetch from "@/services/useFetch";
 import { useRouter } from "expo-router";
-import { Image, ScrollView, View } from "react-native";
+import { ActivityIndicator, Image, ScrollView, Text, View } from "react-native";
 
 export default function Index() {
   const router = useRouter();
@@ -10,6 +12,16 @@ export default function Index() {
   const onPress = () => {
     router.push("/search");
   };
+
+  const {
+    data: movies,
+    loading: moviesLoading,
+    error: moviesError,
+  } = useFetch(() =>
+    fetchMovies({
+      query: "",
+    })
+  );
 
   return (
     <View className="flex-1 bg-primary ">
@@ -23,11 +35,31 @@ export default function Index() {
           paddingBottom: 10,
         }}
       >
+        {/* Logo */}
         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
 
-        <View className=" flex-1 mt-5">
-          <SearchBar onPress={onPress} placeholder="Search for a movie" />
-        </View>
+        {/* Movies List */}
+
+        {moviesLoading ? (
+          <ActivityIndicator
+            size="large"
+            color="#0000ff"
+            className="mt-10 self-center"
+          />
+        ) : moviesError ? (
+          <Text> Error: {moviesError.message}</Text>
+        ) : (
+          <View className=" flex-1 mt-5 ">
+            {/* Search Bar */}
+            <SearchBar onPress={onPress} placeholder="Search for a movie" />
+
+            <>
+              <Text className="text-lg text-white font-bold mt-5 mb-3">
+                Latest Movies{" "}
+              </Text>
+            </>
+          </View>
+        )}
       </ScrollView>
     </View>
   );
--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -9,7 +9,7 @@ interface Props{
 
 const SearchBar = ({onPress, placeholder}: Props) => {
   return (
-    <View className='flex-row items-center bg-dark-200 rounded-full px-5 py-4'>
+    <View className='flex-row items-center bg-dark-200  px-5 py-4 border border-white/10 rounded-full'>
      <Image source={icons.search} className='size-5' resizeMode='contain' tintColor="#ab8bff"/>
 
      <TextInput
@@ -18,7 +18,7 @@ const SearchBar = ({onPress, placeholder}: Props) => {
      value=''
      onChangeText={() => {}}
      placeholderTextColor="#a8b5db"
-     className='flex-1 ml-2 text-white'
+     className='flex-1 ml-2 text-white '
      />
     </View>
   )
