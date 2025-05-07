b925b6e251575a551fa616b007d0436fab46bc30
jeff283 <njorogejeff2019@gmail.com>
2025-05-07 18:19:55 +0300

    feat: implement search functionality with debounce and enhance SearchBar component

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,14 +1,121 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import MovieCard from "@/components/MovieCard";
+import SearchBar from "@/components/SearchBar";
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import { fetchMovies } from "@/services/api";
+import useFetch from "@/services/useFetch";
+import React, { useEffect, useState } from "react";
+import { ActivityIndicator, FlatList, Image, Text, View } from "react-native";
 
 const search = () => {
-  return (
-    <View
-    className="flex-1 items-center justify-center "
-     >
-       <Text className="font-semibold text-3xl text-primary">Search</Text>
-     </View>
-  )
-}
+  const [searchQuery, setSearchQuery] = useState("");
 
-export default search
+  const {
+    data: movies,
+    loading,
+    error,
+    refetch: loadMovies,
+    reset: resetMovies,
+  } = useFetch(
+    () =>
+      fetchMovies({
+        query: searchQuery,
+      }),
+    false // Disable auto-fetching on mount
+  );
+
+  useEffect(() => {
+    const timeoutId = setTimeout(async () => {
+      if (searchQuery.trim()) {
+        // Trigger the fetch when searchQuery changes and is not empty
+        await loadMovies();
+      } else {
+        resetMovies(); // Reset the movies when searchQuery is empty
+      }
+    }, 500); // Debounce for 500ms
+
+    return () => {
+      clearTimeout(timeoutId); // Cleanup the timeout on unmount or when searchQuery changes
+    };
+  }, [searchQuery]);
+
+  return (
+    <View className="flex-1 bg-primary">
+      <Image
+        source={images.bg}
+        className=" absolute w-full z-0"
+        resizeMode="cover"
+      />
+
+      <FlatList
+        data={movies}
+        renderItem={({ item }) => <MovieCard {...item} />}
+        keyExtractor={(item) => item.id.toString()}
+        showsVerticalScrollIndicator={false}
+        numColumns={3}
+        className="px-5"
+        columnWrapperStyle={{
+          justifyContent: "center",
+          gap: 16,
+          marginVertical: 16,
+        }}
+        contentContainerStyle={{
+          paddingBottom: 100,
+        }}
+        ListHeaderComponent={
+          <>
+            <View className="w-full flex-row justify-center mt-20 items-center">
+              <Image source={icons.logo} className="w-12 h-10" />
+            </View>
+
+            <View className="my-5">
+              <SearchBar
+                placeholder="Search for movies..."
+                value={searchQuery}
+                onChangeText={(text: string) => setSearchQuery(text)}
+              />
+            </View>
+
+            {loading && (
+              <ActivityIndicator
+                size="large"
+                color="#0000ff"
+                className="my-3"
+              />
+            )}
+
+            {error && (
+              <Text className="text-red-500 px-5 my-3">
+                Error: {error.message}
+              </Text>
+            )}
+
+            {!loading &&
+              !error &&
+              searchQuery.trim() &&
+              movies?.length! > 0 && (
+                <Text className="text-lg text-white font-bold">
+                  Search Results for{" "}
+                  <Text className="text-accent">{searchQuery}</Text>
+                </Text>
+              )}
+          </>
+        }
+
+        ListEmptyComponent={
+          !loading && !error  ? (
+            <View className="mt-10 px-5">
+              <Text className="text-center text-gray-500">
+                {searchQuery.trim()
+                  ? "No results found. Please try a different search."
+                : "Search for movies..."}
+              </Text>
+            </View>
+          ) : null
+        }
+      />
+    </View>
+  );
+};
+
+export default search;
